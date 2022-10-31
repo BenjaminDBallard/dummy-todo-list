31e5b46e482a2c7dbb0f4351998b25ed02495e3a
BenjaminDBallard <benb971@gmail.com>
2022-10-30 21:36:32 -0500

    edited filters in js

--- todo.js
+++ todo.js
@@ -1,81 +1,112 @@
-    // We'll pre-populate this array with a couple objects just so it's not undefined if your internet connection isn't working properly.
+// We'll pre-populate this array with a couple objects just so it's not undefined if your internet connection isn't working properly.
 
-    let arrayOfTodos = [
-        {
-        "userId": 14,
-        "id": 1,
-        "title": "delectus aut autem",
-        "completed": false
-    },
-    {
-        "userId": 20,
-        "id": 2,
-        "title": "delectus aut autem",
-        "completed": false
-    }]
+let arrayOfTodos = [
+  {
+    userId: 14,
+    id: 1,
+    title: "delectus aut autem",
+    completed: false,
+  },
+  {
+    userId: 20,
+    id: 2,
+    title: "delectus aut autem",
+    completed: false,
+  },
+];
 
-    
-    const fetchTodos = () => {
-        fetch('https://jsonplaceholder.typicode.com/todos')
-        .then( (response) => response.json())
-        .then( (json) => arrayOfTodos = json)
-    }
-    
-    const logTodos = () => {
-        console.log(arrayOfTodos)
-    }
-    let ol = document.getElementById('todo-list')
-    const populateTodos = () => {
-        
+//base variables ----------------------------------------
 
-        for (let i=0; i<arrayOfTodos.length; i++){
-            let newListItem = document.createElement("LI")
-            newListItem.innerText = arrayOfTodos[i].title
-            ol.appendChild(newListItem);
-        }
-    }
+let ol = document.getElementById("todo-list");
 
-    let userIdElement = document.getElementById('userId');
-    let userIdValue = userIdElement.value;
-    console.log('userIdElement:', userIdElement);
-    console.log('userIdValue:', userIdValue);
+//fetches Array -----------------------------------------
 
-    
-    let filteredTodos = arrayOfTodos.filter(todo =>{
-        console.log(todo.userId == userIdValue);
-        return todo.userId == userIdValue;
-    })
-    console.log('fliteredTodos:', filteredTodos)
+const fetchTodos = () => {
+  fetch("https://jsonplaceholder.typicode.com/todos")
+    .then((response) => response.json())
+    .then((json) => (arrayOfTodos = json));
+};
 
-    let complete = arrayOfTodos.filter(todo =>{
-        console.log(todo.completed == true);
-        return todo.completed == true;
-    })
-    console.log('complete:', complete)
+//console logs array ------------------------------------
 
-    let incomplete = arrayOfTodos.filter(todo =>{
-        console.log(todo.completed == false);
-        return todo.completed == false;
-    })
-    console.log('Incomplete:', incomplete)
+const logTodos = () => {
+  console.log(arrayOfTodos);
+};
 
-const filterTodos = (filt) => {
+//clears existing list ----------------------------------
 
-    ol.innerHTML = null;
+const clearList = () => {
+  //clear existing list
+  ol.remove();
+  let olContainer = document.getElementById("list-container");
+  olContainer.innerHTML = "<ol id='todo-list'></ol>";
+  ol = document.getElementById("todo-list");
+};
 
-        for (let i=0; i<filterTodos.length; i++){
-            let newListItem = document.createElement("LI")
-            newListItem.innerText = filterTodos[i].title
-            ol.appendChild(newListItem);
-        }
-}
+//populates list ----------------------------------------
 
+const populateTodos = () => {
+  clearList();
 
+  for (let i = 0; i < arrayOfTodos.length; i++) {
+    let newListItem = document.createElement("LI");
+    newListItem.innerText = arrayOfTodos[i].title;
+    ol.appendChild(newListItem);
+  }
+};
 
+//filter ------------------------------------------------
 
-    // get ol element to put todos in
-    // loop throuh arrayOfTodos
-    // create li element
-    // create text node to put title proporty
-    // append text to li element
-    // append li element to ol
+const filterTodos = () => {
+  clearList();
+
+  let userIdElement = document.getElementById("userId");
+  console.log("userIdElement:", userIdElement);
+
+  let userIdValue = userIdElement.value;
+  console.log("userIdValue:", userIdValue);
+
+  let filteredTodos = arrayOfTodos.filter((todo) => {
+    console.log(todo.userId == userIdValue);
+    return todo.userId == userIdValue;
+  });
+  console.log("fliteredTodos:", filteredTodos);
+
+  for (let i = 0; i < filteredTodos.length; i++) {
+    let newListItem = document.createElement("LI");
+    newListItem.innerText = filteredTodos[i].title;
+    ol.appendChild(newListItem);
+  }
+};
+
+const completed = () => {
+  clearList();
+
+  let complete = arrayOfTodos.filter((todo) => {
+    console.log(todo.completed == true);
+    return todo.completed == true;
+  });
+  console.log("complete:", complete);
+
+  for (let i = 0; i < complete.length; i++) {
+    let newListItem = document.createElement("LI");
+    newListItem.innerText = complete[i].title;
+    ol.appendChild(newListItem);
+  }
+};
+
+const incompleted = () => {
+  clearList();
+
+  let incomplete = arrayOfTodos.filter((todo) => {
+    console.log(todo.completed == false);
+    return todo.completed == false;
+  });
+  console.log("Incomplete:", incomplete);
+
+  for (let i = 0; i < incomplete.length; i++) {
+    let newListItem = document.createElement("LI");
+    newListItem.innerText = incomplete[i].title;
+    ol.appendChild(newListItem);
+  }
+};
